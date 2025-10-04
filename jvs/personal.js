43b7fef6137d841b671f9358f2bba735c7b8f900
vhieu2k5 <vhieeu2005@gmail.com>
2025-10-04 21:19:11 +0700

    update JVS 4/10

--- jvs/personal.js
+++ jvs/personal.js
@@ -4,6 +4,19 @@ const section2 = document.querySelector(".section-2");
 const section3 = document.querySelector(".section-3");
 const profileBtn = document.getElementById("profileBtn");
 const passwordBtn = document.getElementById("passwordBtn");
+//Start the code
+  alltheSections.forEach(element => {
+        element.style.display="none";
+    });
+section1.style.display="flex";
+//Nút Hồ sơ
+profileBtn.onclick = function(){
+    alltheSections.forEach(element => {
+    element.style.display="none";
+    });
+section1.style.display="flex";
+}
+//Nút bảo mật và Mật khẩu
 passwordBtn.onclick = function(){
     alltheSections.forEach(element => {
         element.style.display="none";
