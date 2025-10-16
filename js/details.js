70b81ffbe475836a5005b3112ff1af96bc690e79
trongnhan070105 <trongnhan070105@gmail.com>
2025-10-16 15:53:20 +0700

    update them chuc nang thu nhat

---
+++ js/details.js
@@ -0,0 +1,43 @@
+const heartIcon = document.querySelector(".datvaicon i.fa-heart");
+const darkPanel = document.querySelector(".darkpanel");
+const closeFavBtn = document.getElementById("closefav");
+const favPanel = document.querySelector(".favouritebox");
+const unfavPanel = document.querySelector(".unfavouritebox");
+const confirmRemove = document.getElementById("confirmRemove");
+const cancelRemove = document.getElementById("cancelRemove");
+let liked = false;
+
+if (heartIcon) {
+    darkPanel.style.display = "none";
+    heartIcon.addEventListener("click", () => {
+        darkPanel.style.display = "flex";
+        if (!liked) {
+            favPanel.style.display = "flex";
+            unfavPanel.style.display = "none";
+
+            heartIcon.classList.toggle("fa-solid");
+            heartIcon.style.color = "red";
+            liked = true;
+        }
+        else {
+            unfavPanel.style.display = "flex";
+            favPanel.style.display = "none";
+        }
+    })
+}
+
+if (closeFavBtn) {
+    closeFavBtn.addEventListener("click", () => {
+        darkPanel.style.display = "none";
+
+    })
+}
+confirmRemove.onclick = function () {
+    liked = false; 
+    heartIcon.classList.remove("fa-solid");
+    heartIcon.style.color = "black";
+    darkPanel.style.display = "none";
+}
+cancelRemove.onclick = function () {
+    darkPanel.style.display = "none";
+}
