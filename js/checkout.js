723e7f66c0a978f0fd4fe019aae0a4f87e59fbba
ndlliinh <ngphuongmyy@gmail.com>
2025-10-28 21:39:13 +0700

    còn phần hủy tour nữa chờ vur kiên còn lại xog r nha

---
+++ js/checkout.js
@@ -0,0 +1,36 @@
+
+document.addEventListener('DOMContentLoaded', () => {
+
+    const datNgay = document.getElementById('dat-ngay');
+    const cardRadio = document.getElementById('pay-card');
+    const walletRadio = document.getElementById('pay-wallet');
+    const overlay = document.getElementById('backdrop-overlay');
+    const visaPopup = document.getElementById('visa-popup');
+    const paypalPopup = document.getElementById('paypal-popup');
+    
+    datNgay.addEventListener('click', (event) => {
+        event.preventDefault(); 
+        
+        let popupElementToShow = null;
+        
+        if (cardRadio.checked) {
+            popupElementToShow = visaPopup; 
+        } else if (walletRadio.checked) {
+            popupElementToShow = paypalPopup;
+        }
+        
+        if (popupElementToShow) {
+            showPopup(popupElementToShow);
+        }
+    });
+
+
+    function showPopup(popupElement) {
+        
+        overlay.classList.add('show');
+        popupElement.classList.add('show');
+        setTimeout(() => {
+            window.location.href = "complete-booking.html";
+        }, 2000);
+    }
+});
