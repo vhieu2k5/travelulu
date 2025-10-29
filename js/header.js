4979a8ba1e078fd631c92642a3420e0ccd5d03bd
huyn46557-png <huyn46557@gmail.com>
2025-10-29 19:32:03 +0700

    final

--- js/header.js
+++ js/header.js
@@ -1,7 +1,34 @@
-// Load header tự động
 document.addEventListener("DOMContentLoaded", () => {
   const includes = document.querySelectorAll("[data-include]");
+
+  const chatbot = document.querySelectorAll("[chatBot]");
   includes.forEach(el => {
+    const file = el.getAttribute("data-include");
+    fetch(file)
+      .then(res => {
+        if (!res.ok) throw new Error(`Không thể tải ${file}`);
+        return res.text();
+      })
+      .then(data => {
+        el.innerHTML = data;
+
+        //chèn xong header rồi mới update link
+        const personalLink = document.getElementById("personal-site");
+        if (personalLink) {
+          if (Loader.isLoaded) {
+            personalLink.href = "personal.html";
+            personalLink.textContent = "Cá nhân";
+          } else {
+            personalLink.href = "login.html";
+            personalLink.textContent = "Đăng nhập";
+          }
+        }
+      })
+      .catch(err => console.error("Lỗi include:", err));
+  });
+
+  //Cho chatbox
+   chatbot.forEach(el => {
     const file = el.getAttribute("data-include");
     fetch(file)
       .then(res => {
@@ -14,3 +41,13 @@ document.addEventListener("DOMContentLoaded", () => {
       .catch(err => console.error("Lỗi include:", err));
   });
 });
+
+
+class Loader {
+  static get isLoaded() {
+    return sessionStorage.getItem("isLoaded") === "true";
+  }
+  static set isLoaded(value) {
+    sessionStorage.setItem("isLoaded", value);
+  }
+}
